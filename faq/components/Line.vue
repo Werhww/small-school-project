f8e8dfd4bcb7c4c4bc848460de95c95c2410ad35
werhw <kleppeleo@gmail.com>
2023-10-05 10:25:07 +0200

    faq page finished

---
+++ faq/components/Line.vue
@@ -0,0 +1,20 @@
+<template>
+<hr/>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+    opacity: number
+}>(), {
+    opacity: 0.1
+})
+</script>
+
+<style scoped>
+hr {
+    background-color: #000;
+    opacity: v-bind(opacity);
+    width: 100%;
+    height: 2px;
+}
+</style>
