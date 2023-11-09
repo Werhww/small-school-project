be05f5938ca5e5894dd06d355355a048bedca4cf
werhw <kleppeleo@gmail.com>
2023-11-09 13:19:03 +0100

    Endret mappe navn på prosjekter

--- SqLite/index.ts
+++ SqLite/index.ts
@@ -1,33 +1,58 @@
-import express from 'express'
-import { PrismaClient, User } from '@prisma/client';
-import { join } from 'path';
+import express from "express"
+import { PrismaClient, User, Post } from "@prisma/client";
+import { join } from "path";
 
 const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
 
+const publicFiles = join(__dirname, "public");
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(join(__dirname, 'public')))
-
-
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}/`);
 })
 
-app.get('/', (req, res) => {
-    res.sendFile('/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(join("index.html"), {root: publicFiles});
+});
+
+app.get("/script.js", (req, res) => {  
+    res.sendFile(join("script.js"), {root: publicFiles}) 
 });
 
 
-app.get('api/users', (req, res) => {
+app.post("/api/user", async (req, res) => {
     const body = req.body as User
 
-    const user = prisma.user.create({
+    const user = await prisma.user.create({
         data: body
+    }).catch((err) => {
+        res.status(500).json(err)
     })
 
-    res.json(user);
-});
+    console.log(user, body)
+
+    res.json(user)
+})
+
+app.post("/api/post", async (req, res) => {
+    const body = req.body as Post
+    
+    const post = await prisma.post.create({
+        data: {
+            authorId: Number(body.authorId),
+            title: body.title,
+        }
+    }).catch((err) => {
+        res.status(500).json(err)
+        console.log(err)
+    })
+
+    console.log(post, body)
+
+    res.json(post)
+})
--- SqLite/public/script.js
+++ SqLite/public/script.js
@@ -1,30 +1,51 @@
-const form = document.getElementById('createUserForm');
+const form = document.getElementById("createUserForm")
+const form2 = document.getElementById("createPostForm")
 
-
-async function request(path, body) {
-    return fetch('/api/users', {
-        method: 'POST',
+async function request(path, method, body) {
+    return fetch(path, {
+        method: method,
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify(body)
     })
 }
 
-async function createUser() {
-    const newUser = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({
-            name: name.value,
-            email: email.value
-        }),
-    })
+async function createUser(e) {
+    e.preventDefault()
+    const formData = new FormData(form);
+    const body = Object.fromEntries(formData);
+    if (body.email === "") {
+        alert("Missing email")
+        return
+    }
+
+    const newUser = await request("/api/user", "POST", body)
 
     const response = await newUser.json()
 
     console.log(response)
-
-    const formData = new FormData(request);
 }
 
-submit.addEventListener('click', createUser);
+async function createPost(e) {
+    e.preventDefault()
+    const formData = new FormData(form2);
+    const body = Object.fromEntries(formData);
+
+    if (body.authorId === "" || body.title === "") {
+        alert("Missing authorId or title")
+        return
+    }
+
+    console.log(body)
+
+
+    const newPost = await request("/api/post", "POST", body)
+
+    const response = await newPost.json()
+
+    console.log(response)
+}
+
+form.addEventListener("submit", createUser)
+form2.addEventListener("submit", createPost)
