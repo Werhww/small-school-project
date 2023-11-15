7a100057e215633a7255e0a37c49573d50df6838
werhw <kleppeleo@gmail.com>
2023-11-15 16:16:50 +0100

    Finished prisma schema and conneted postgresdb

---
+++ buekorpsportalen/pages/auth.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<p>eple</p>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ buekorpsportalen/pages/dashbord/[dashbord].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const dashbordId = useRoute().params.dashbord
+
+</script>
+
+<template>
+<p>{{ dashbordId }}</p>
+</template>
+
+<style scoped lang="scss">
+
+</style>
