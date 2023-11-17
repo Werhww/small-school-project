b743da5d9112cddea827d609adda3afe9ef90ae7
werhw <kleppeleo@gmail.com>
2023-11-17 13:47:50 +0100

    Remove unused dashboard component and fix typo in schema.prisma

---
+++ buekorpsportalen/pages/dashboard/[dashboard].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const dashboardId = useRoute().params.dashboard
+
+</script>
+
+<template>
+<p>{{ dashboardId }}</p>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- buekorpsportalen/pages/index.vue
+++ buekorpsportalen/pages/index.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-const dashbordId = useRoute().params.dashbord
 
 </script>
 
 <template>
-<p>{{ dashbordId }}</p>
+
 </template>
 
 <style scoped lang="scss">
